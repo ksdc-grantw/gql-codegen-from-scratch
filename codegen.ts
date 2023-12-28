d15326d97e997e5ea4e3399786c396cbfa1a693f
Grant Walker <grant.walker@ksdc.co.za>
2023-12-28 09:14:54 +0200

    configure to generate hooks

--- codegen.ts
+++ codegen.ts
@@ -4,9 +4,13 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://rickandmortyapi.com/graphql",
+  documents: "./**/*.graphql",
   generates: {
     "src/generated/graphql.ts": {
-      plugins: ["typescript"]
+      plugins: ['typescript-operations', 'typescript-react-apollo'],
+      config: {
+        withHooks: true
+      },
     }
   }
 };
